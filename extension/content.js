6fdd6fd96a9644e26df74fd677ee2b25c92bd389
ahmadgaz <gazali.contact@gmail.com>
2024-04-27 15:36:35 -0700

    Added mutation observer

--- extension/content.js
+++ extension/content.js
@@ -44,4 +44,56 @@ function highlightTextNodes(rootNode) {
   textNodes.forEach((node) => highlightNode(node));
 }
 
-highlightTextNodes(document.body);
+// Mutation Observer
+
+// I ran into a bug that a debounced and time limited observer callback
+// fixes (specifically on the the page you're redirected to when you
+// click the "Edit this file" button on any file in a github repo).
+//
+// The bug caused the MutationObserver to detect mutations on an
+// infinite loop. Once a mutation is detected on an element, any
+// text node within the element is highlighted, but then the element
+// is mutated again to replace the highlighted text with plain text
+// (maybe by some script on the original page).
+function debouncedCallback(callback, wait, limit, limitTimeWindow) {
+  let timerId;
+  let callCount = 0;
+  let startTime = Date.now();
+  return (...args) => {
+    const now = Date.now();
+
+    // Reset count and if the time window has expired
+    if (now - startTime > limitTimeWindow) {
+      startTime = now;
+      callCount = 0;
+    }
+
+    // Check if the function has been called more times than the limit within the time window
+    if (callCount >= limit) {
+      clearTimeout(timerId);
+      return;
+    }
+
+    callCount++;
+
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      callback(...args);
+    }, wait);
+  };
+}
+
+const rootNode = document.body;
+const observerConfig = { attributes: true, childList: true, subtree: true };
+const observerCallback = debouncedCallback(
+  (mutationList) => {
+    for (const mutation of mutationList) {
+      highlightTextNodes(mutation.target);
+    }
+  },
+  50,
+  100,
+  5000,
+);
+const observer = new MutationObserver(observerCallback);
+observer.observe(rootNode, observerConfig);
