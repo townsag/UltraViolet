573ec07b005a9bcc0ec0d23f2b924d0042d42449
townsag <townsag@gmail.com>
2024-04-27 23:51:22 -0700

    reordered some of the function definitions. Added a object factory that creates an object that the hilight matching sentances method can use to classify sentances

--- extension/content.js
+++ extension/content.js
@@ -27,6 +27,7 @@ const tagsToIgnore = [
   "time",
   "var",
   "video",
+  "footer",
 ];
 
 const htmlPhrasingTags = [
@@ -90,13 +91,153 @@ function isValidTextNode(node) {
   );
 }
 
-function isHighlightElement(element) {
-  return element.className === "highlight";
-}
-
 function isPhrasingNode(element) {
   return htmlPhrasingTags.includes(element.tagName.toLowerCase());
 }
+
+function getParagraphNodes(currentNode) {
+  if (tagsToIgnore.includes(currentNode.tagName.toLowerCase())) return [];
+  let isParagraphNode = Array.from(currentNode.childNodes).every((node) => {
+    return (
+      node.nodeType === Node.TEXT_NODE ||
+      (node.nodeType === Node.ELEMENT_NODE && isPhrasingNode(node))
+    );
+  });
+
+  if (isParagraphNode) {
+    //console.log("isPara", currentNode);
+    return [currentNode];
+  } else {
+    let children = Array.from(currentNode.children);
+    let paragraphNodes = [];
+    //console.log("NotPara", children);
+    for (let i = 0; i < children.length; i++) {
+      paragraphNodes.push(...getParagraphNodes(children[i]));
+    }
+    return paragraphNodes;
+  }
+}
+
+// let paragraphNodes = getParagraphNodes(document.body);
+
+function createSpan(textContent, color) {
+    let hilightedTextNode = document.createElement("span");
+    hilightedTextNode.textContent = textContent;
+    hilightedTextNode.style.backgroundColor = color;
+    return hilightedTextNode;
+}
+
+function classifierFactory(redList, apiEndpoint) {
+    return {
+        // return true if any of the redlist words are found
+        classify(sentance) {
+            return redList.some((word) => {
+                return sentance.includes(word);
+            });
+        }
+    }
+}
+
+
+function hilightMatchingSentances(classifier, paragraphNode){
+    let sentanceBoundaryReg = /(?<=[.?!]+)/;
+    // buffer holds objects with attributes node and text
+    let buffer = []
+    let paragraphNodeIterator = document.createNodeIterator(paragraphNode, NodeFilter.SHOW_TEXT);
+    let currentSubNode = paragraphNodeIterator.nextNode();
+    while(currentSubNode){
+        let next_node = paragraphNodeIterator.nextNode();
+        // ToDo: gunna need to add some checks for empty textContent nodes here
+        // this is the case that the current sub node text does not contain any sentance delimeters
+        if (!sentanceBoundaryReg.test(currentSubNode.textContent)) {
+            buffer.push({node:currentSubNode, text:currentSubNode.textContent});
+        // this is the case that the current sub node contains one or more sentance delimeters
+        } else {
+            // if a text node contains multiple sentances. Split the text content into multiple sentances then 
+            // replace the original textContent node with a group of text content nodes, one for each sub sentance.
+            // replace all the textContent nodes associated with sentances in the buffer first
+            let sentances = currentSubNode.textContent.split(sentanceBoundaryReg);
+            let currentSentance = buffer.map(pair => pair.text).join("") + sentances[0];
+            // flush the buffer
+            if (classifier.classify(currentSentance)) {
+                // replace the nodes in the buffer with hilighted nodes
+                for (const pair of buffer) {
+                    hilightedSpan = createSpan(pair.text, "yellow");
+                    pair.node.parentNode.replaceChild(hilightedSpan, pair.node);
+                }
+            }
+            buffer = [];
+
+            // now that the nodes in the buffer have been handled, handle the sentances in currentNode
+            // we split the current textNode with multiple sentances into multiple text nodes each with just one sentance
+            // in this case the first string in sentances will always end in a punctuation character
+            let newFragment = document.createDocumentFragment();
+            // handle the first sentance in sentances, garanteed to have punctuation
+            if (classifier.classify(currentSentance)) {
+                // add the first sentance to our new nodes array as a hilighted node
+                hilightedSpan = createSpan(sentances[0], "yellow");
+                newFragment.append(hilightedSpan);
+            } else {
+                newFragment.append(document.createTextNode(sentances[0]));
+            }
+
+            // handle all sentances but the first and last sentance in sentances, garanteed to have punctuation
+            for (const middleSentance of sentances.slice(1, -1)) {
+                if (classifier.classify(middleSentance)){
+                    hilightedSpan = createSpan(middleSentance, "yellow");
+                    newFragment.append(hilightedSpan);
+                } else {
+                    newFragment.append(document.createTextNode(middleSentance));
+                }
+            }
+
+            // handle the last sentance in sentances only if sentances is greater than length one
+            // if sentances is greater than length one then the last sentance is not garanteed to have punctuation
+            if (sentances.length > 1) {
+                let lastSentance = sentances[sentances.length - 1];
+                if (sentanceBoundaryReg.test(lastSentance)) {
+                    // handle the case where the last string in the sentance is a complete sentance
+                    if (classifier.classify(lastSentance)) {
+                        // add a highligted span to the list of nodes
+                        hilightedSpan = createSpan(lastSentance, "yellow");
+                        newFragment.append(hilightedSpan);
+                    } else {
+                        newFragment.append(document.createTextNode(lastSentance));
+                    }
+                } else {
+                    // handle the case where the last string in sentances is a sentance fragment
+                    sentanceFragmentNode = document.createTextNode(lastSentance);
+                    newFragment.append(sentanceFragmentNode);
+                    buffer.push({node:sentanceFragmentNode, text:lastSentance})
+                }
+            }
+
+            // replace the text node from this sentance with the dom fragment
+            currentSubNode.parentNode.replaceChild(newFragment, currentSubNode);
+        }
+        currentSubNode = next_node;
+    }
+
+    // classify and hilight any remaining nodes in the buffer
+    if (buffer.length > 0) {
+        let currentSentance = buffer.map(pair => pair.text).join("");
+        if (classifier.classify(currentSentance)){
+            for (const pair of buffer) {
+                hilightedSpan = createSpan(pair.text, "yellow");
+                pair.node.parentNode.replaceChild(hilightedSpan, pair.node);
+            }
+        }
+    }
+}
+
+// let classifier = classifierFactory(["bone"], "asdf");
+// for (const node of paragraphNodes) {
+//     hilightMatchingSentances(classifier, node);
+// }
+
+function isHighlightElement(element) {
+    return element.className === "highlight";
+  }
 
 function getTextNodes(rootNode) {
   const nodeIterator = document.createNodeIterator(
@@ -125,128 +266,6 @@ function highlightNode(node) {
   span.className = "highlight";
   span.textContent = node.nodeValue;
   node.parentNode.replaceChild(span, node);
-}
-
-function getParagraphNodes(currentNode) {
-  if (tagsToIgnore.includes(currentNode.tagName.toLowerCase())) return [];
-  let isParagraphNode = Array.from(currentNode.childNodes).every((node) => {
-    return (
-      node.nodeType === Node.TEXT_NODE ||
-      (node.nodeType === Node.ELEMENT_NODE && isPhrasingNode(node))
-    );
-  });
-
-  if (isParagraphNode) {
-    //console.log("isPara", currentNode);
-    return [currentNode];
-  } else {
-    let children = Array.from(currentNode.children);
-    let paragraphNodes = [];
-    //console.log("NotPara", children);
-    for (let i = 0; i < children.length; i++) {
-      paragraphNodes.push(...getParagraphNodes(children[i]));
-    }
-    return paragraphNodes;
-  }
-}
-
-function createSpan(textContent, color) {
-    let hilightedTextNode = document.createElement("span");
-    hilightedTextNode.textContent = textContent;
-    hilightedTextNode.style.backgroundColor = color;
-    return hilightedTextNode;
-}
-
-let dogReg = /dog/;
-function hilightMatchingSentances(regex, paragraphNode){
-    let sentanceBoundaryReg = /(?<=[.?!]+)/;
-    // buffer holds objects with attributes node and text
-    let buffer = []
-    let paragraphNodeIterator = document.createNodeIterator(paragraphNode, NodeFilter.SHOW_TEXT);
-    let currentSubNode = paragraphNodeIterator.nextNode();
-    while(currentSubNode){
-        let next_node = paragraphNodeIterator.nextNode();
-        // ToDo: gunna need to add some checks for empty textContent nodes here
-        // this is the case that the current sub node text does not contain any sentance delimeters
-        if (!sentanceBoundaryReg.test(currentSubNode.textContent)) {
-            buffer.push({node:currentSubNode, text:currentSubNode.textContent});
-        // this is the case that the current sub node contains one or more sentance delimeters
-        } else {
-            // if a text node contains multiple sentances. Split the text content into multiple sentances then 
-            // replace the original textContent node with a group of text content nodes, one for each sub sentance.
-            // replace all the textContent nodes associated with sentances in the buffer first
-            let sentances = currentSubNode.textContent.split(sentanceBoundaryReg);
-            let currentSentance = buffer.map(pair => pair.text).join("") + sentances[0];
-            // flush the buffer
-            if (regex.test(currentSentance)) {
-                // replace the nodes in the buffer with hilighted nodes
-                for (const pair of buffer) {
-                    hilightedSpan = createSpan(pair.text, "yellow");
-                    pair.node.parentNode.replaceChild(hilightedSpan, pair.node);
-                }
-            }
-            buffer = [];
-
-            // now that the nodes in the buffer have been handled, handle the sentances in currentNode
-            // we split the current textNode with multiple sentances into multiple text nodes each with just one sentance
-            // in this case the first string in sentances will always end in a punctuation character
-            let newFragment = document.createDocumentFragment();
-            // handle the first sentance in sentances, garanteed to have punctuation
-            if (regex.test(currentSentance)) {
-                // add the first sentance to our new nodes array as a hilighted node
-                hilightedSpan = createSpan(sentances[0], "yellow");
-                newFragment.append(hilightedSpan);
-            } else {
-                newFragment.append(document.createTextNode(sentances[0]));
-            }
-
-            // handle all sentances but the first and last sentance in sentances, garanteed to have punctuation
-            for (const middleSentance of sentances.slice(1, -1)) {
-                if (regex.test(middleSentance)){
-                    hilightedSpan = createSpan(middleSentance, "yellow");
-                    newFragment.append(hilightedSpan);
-                } else {
-                    newFragment.append(document.createTextNode(middleSentance));
-                }
-            }
-
-            // handle the last sentance in sentances only if sentances is greater than length one
-            // if sentances is greater than length one then the last sentance is not garanteed to have punctuation
-            if (sentances.length > 1) {
-                let lastSentance = sentances[sentances.length - 1];
-                if (sentanceBoundaryReg.test(lastSentance)) {
-                    // handle the case where the last string in the sentance is a complete sentance
-                    if (regex.test(lastSentance)) {
-                        // add a highligted span to the list of nodes
-                        hilightedSpan = createSpan(lastSentance, "yellow");
-                        newFragment.append(hilightedSpan);
-                    } else {
-                        newFragment.append(document.createTextNode(lastSentance));
-                    }
-                } else {
-                    // handle the case where the last string in sentances is a sentance fragment
-                    sentanceFragmentNode = document.createTextNode(lastSentance);
-                    newFragment.append(sentanceFragmentNode);
-                    buffer.push({node:sentanceFragmentNode, text:lastSentance})
-                }
-            }
-
-            // replace the text node from this sentance with the dom fragment
-            currentSubNode.parentNode.replaceChild(newFragment, currentSubNode);
-        }
-        currentSubNode = next_node;
-    }
-
-    // classify and hilight any remaining nodes in the buffer
-    if (buffer.length > 0) {
-        let currentSentance = buffer.map(pair => pair.text).join("");
-        if (regex.test(currentSentance)){
-            for (const pair of buffer) {
-                hilightedSpan = createSpan(pair.text, "yellow");
-                pair.node.parentNode.replaceChild(hilightedSpan, pair.node);
-            }
-        }
-    }
 }
 
 function highlightTextNodes(rootNode) {
